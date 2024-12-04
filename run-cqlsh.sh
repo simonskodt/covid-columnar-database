29193f860f41a298d5096876b971d8d4bf44135c
Simon Johann Skødt <simon@skodt.dk>
2024-12-04 14:13:45 +0100

    feat: add extra function

--- run-cqlsh.sh
+++ run-cqlsh.sh
@@ -18,9 +18,22 @@ while getopts "ta" opt; do
     esac
 done
 
-# Function to execute CQL file
 execute_cql() {
     docker exec -it cassandra cqlsh -f /data.cql > /dev/null 2>&1
+}
+
+execute_remaining_cql() {
+    # Execute the COPY command for the covid table
+    if [ "$use_test_file" = false ]; then
+        echo "Executing COPY command..."
+        docker exec -it cassandra cqlsh -e "
+        COPY covid.countries_aggregated (id, date, country, confirmed, recovered, deaths) 
+        FROM '/countries-aggregated-with-uuid.csv' WITH HEADER = TRUE;"
+    fi
+
+    # Staring interactive session in terminal
+    echo "\033[1;32m\nSUCCESSFUL COPY\033[0m \n\n\033[1mStarting interactive CQL session...\033[0m"
+    docker exec -it cassandra cqlsh
 }
 
 waiting_ui() {
@@ -46,24 +59,16 @@ if [ "$auto_retry" = true ]; then
     done
     kill $ui_pid
     echo  "\n\033[1;32m\nCONNECTION READY\033[0m \n\n\033[1mProceeding with next steps...\033[0m"
+    execute_remaining_cql
 else
     echo "Executing CQL inside container for .cql file..."
     execute_cql
+
     if [ $? -ne 0 ]; then
         echo "\033[1;31m\nCONNECTION ERROR: Unable to connect to any servers.\033[0m\n\033[1m> Try again in a short time.\033[0m"
         exit 1
     fi
     echo "\033[1;32m\nCONNECTION READY\033[0m \n\n\033[1mProceeding with next steps...\033[0m"
 
-    # Execute the COPY command for the covid table
-    if [ "$use_test_file" = false ]; then
-        echo "Executing COPY command..."
-        docker exec -it cassandra cqlsh -e "
-        COPY covid.countries_aggregated (id, date, country, confirmed, recovered, deaths) 
-        FROM '/countries-aggregated-with-uuid.csv' WITH HEADER = TRUE;"
-    fi
-
-    # Staring interactive session in terminal
-    echo "\033[1;32m\nSUCCESSFUL COPY\033[0m \n\n\033[1mStarting interactive CQL session...\033[0m"
-    docker exec -it cassandra cqlsh
+    execute_remaining_cql
 fi
