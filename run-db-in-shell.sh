c3d3c97e66ec3a5ad9daccad24d96f08bf38c61e
Simon Johann Skødt <simon@skodt.dk>
2024-12-04 09:21:01 +0100

    feat: improve run-db-in-shell script to execute CQL files and start interactive session

--- run-db-in-shell.sh
+++ run-db-in-shell.sh
@@ -1,2 +1,6 @@
+echo "Executing CQL inside container for .cql file..."
 docker exec -it cassandra cqlsh -f /data.cql
+sleep 1
+
+echo "Starting interactive CQL session..."
 docker exec -it cassandra cqlsh
