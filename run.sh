d161c60b9cade53c21a7993d3687161d9d990825
Simon Johann Skødt <simon@skodt.dk>
2024-12-04 09:21:09 +0100

    feat: enhance run.sh to create Docker network and improve container startup process

--- run.sh
+++ run.sh
@@ -1,10 +1,18 @@
+network_output=$(docker network create cassandra 2>&1)
+
+if [ network_output == "Error response from daemon: network with name cassandra already exists" ]; then
+    echo "Network 'cassandra' already exists."
+else
+    echo "Network 'cassandra' created."
+fi
+
 if [ -z "$(docker ps -q -f name=cassandra)" ]; then
     echo "Cassandra container is not running. Starting a new one..."
-    docker run --name cassandra -d cassandra
+    docker run --rm -d --name cassandra --hostname cassandra --network cassandra -p 9042:9042 cassandra
+    echo "Cassandra has started."
 else
     echo "Cassandra container is already running."
 fi
 
+echo "Coping file into container..."
 docker cp test-setup/data.cql cassandra:/data.cql
-docker exec -it cassandra cqlsh -f /data.cql
-docker exec -it cassandra cqlsh
