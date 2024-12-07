d479ad23302ca2254e2a0973bb9d6d7dfe664fa1
Simon Johann Skødt <simon@skodt.dk>
2024-12-08 00:05:59 +0100

    feat(query6): update queries and schema for total deaths by continent

--- run-cqlsh.sh
+++ run-cqlsh.sh
@@ -56,9 +56,9 @@ execute_remaining_cql() {
             COPY covid.province_info_by_admin2 (uid, iso3, admin2, province_state, lat, long, population) 
             FROM '/reference-filtered.csv' WITH HEADER = TRUE;"
 
-        # 5) countries_by_continents
+        # 5) country_total_deaths_by_continent
         docker exec -it cassandra cqlsh -e "
-            COPY covid.countries_by_continents (continent, country, total_confirmed, total_recovered, total_deaths) 
+            COPY covid.country_total_deaths_by_continent (continent, country, total_confirmed, total_recovered, total_deaths) 
             FROM '/countries-aggregated-sum-continents.csv' WITH HEADER = TRUE;"
 
         # 6) covid_first_day
